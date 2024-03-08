934c5c1061f22273d48c03e8f0c2214b1ccf7c0e
Nick Masella <142621287+NickMasella@users.noreply.github.com>
2024-03-07 20:29:39 -0600

    init

---
+++ src/main/java/org/launchcode/codecallers/CodeCallersApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.codecallers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCallersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCallersApplication.class, args);
+	}
+
+}
