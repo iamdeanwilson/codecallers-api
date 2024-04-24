dba4d1f8c24025e0d6c449d0e969e7bf11750c45
ngarner98 <exmachina091098@gmail.com>
2024-04-24 02:17:04 -0500

    Functionality to query QuizHistory table from the front end

--- src/main/java/org/launchcode/codecallers/models/data/QuizRepository.java
+++ src/main/java/org/launchcode/codecallers/models/data/QuizRepository.java
@@ -4,8 +4,6 @@ import org.launchcode.codecallers.models.QuizHistory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface QuizRepository extends CrudRepository<QuizHistory, Integer> {
 
